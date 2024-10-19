ae0950422e769f06eebecf4ae9d0c76f8efe9dc9
SquireofNowhere <140905424+SquireofNowhere@users.noreply.github.com>
2024-10-19 12:48:31 +0200

    added login tings
    username persisting,
    basic user login check,
    plan to implement cookies for password tings

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,1 +1,44 @@
 console.log("testing testing.....")
+
+
+//faux database for testing
+const users = {
+    'Mckyle' : 'Innit321',
+    'Vutomi' : 'bitty2',
+    'him' : 'letmein',
+    'Nk' : 'password'
+}
+
+//index.html
+//handle login submission from form
+try {
+    document.getElementById('loginform').addEventListener('submit',function(event) {
+        event.preventDefault();
+    
+        
+        try {
+            
+            const username = document.getElementById('username').value
+            const password = document.getElementById('password').value    
+    
+            if (users[username] && users[username] === password){
+    
+                window.location.href = 'html/Main.html';
+                sessionStorage.setItem('username', username)
+            }else{
+                alert('Invalid username or password!')
+            }
+    
+        } catch (error) {
+            alert("!ERROR!\n" + error);
+        }
+    
+        
+        
+       
+    });
+} catch (error) {
+    
+}
+
+
