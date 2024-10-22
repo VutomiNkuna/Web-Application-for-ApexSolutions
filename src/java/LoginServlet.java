efb9dd0c0b7e26a2cf3a6658fb4b325c8206d03b
SquireofNowhere <140905424+SquireofNowhere@users.noreply.github.com>
2024-10-22 14:18:29 +0200

    the work ive done till now. struggling to get things to connect, might go look at notes

--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -5,10 +5,14 @@ import jakarta.servlet.annotation.WebServlet;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;abstract 
+import jakarta.servlet.http.HttpSession;
+abstract 
         
-        
+
+@WebServlet("/login")        
 public class LoginServlet extends HttpServlet {
+    
+    
     
     protected void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException {
         
@@ -18,5 +22,17 @@ public class LoginServlet extends HttpServlet {
         
         String validUsername = "admin";
         String validPassword = "password123";
+        
+        if (validUsername.equals(username) && validPassword.equals(password)){
+            
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            
+            response.sendRedirect("Main.jsp");
+        } else {
+            System.out.print("ERRORR WRONG!!!");
+            
+            
+        }
     }
 }
--- web/javascript/Mainlogic.js
+++
@@ -1,4 +0,0 @@
-console.log("testing testing Main..")
-
-//Main.html
-document.getElementById('Hello').innerHTML ="Hello " + sessionStorage.getItem('username')
